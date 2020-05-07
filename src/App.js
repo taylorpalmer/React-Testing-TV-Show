1ad298c1641d97af30c986d15b35f34fc5db8d7f
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-05-06 18:11:13 -0700

    refactored axios call into separate file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
-
+import fetchShow from "./api/fetchShow";
 import Episodes from "./components/Episodes";
 import "./styles.css";
 
@@ -15,20 +14,15 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
+    fetchShow().then((res) => {
+      setShow(res.data);
+      // @ts-ignore
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+    });
     fetchShow();
   }, []);
 
-  const handleSelect = e => {
+  const handleSelect = (e) => {
     setSelectedSeason(e.value);
   };
 
